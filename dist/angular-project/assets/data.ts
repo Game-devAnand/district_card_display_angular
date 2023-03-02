ab6b9838e477c65f10ea8ea17e9ff8508465cc97
Game-devAnand <114950258+Game-devAnand@users.noreply.github.com>
2023-03-02 19:55:31 +0530

    first commit

---
+++ dist/angular-project/assets/data.ts
@@ -0,0 +1,59 @@
+export const district = [
+    {
+        name : "Thiruvananthapuram",
+        img : ""
+    },
+    {
+        name : "Kollam",
+        img : ""
+    },
+    {
+        name : "Pathanamthitta",
+        img : ""
+    },
+    {
+        name : "Alappuzha",
+        img : ""
+    },
+    {
+        name : "Kottayam",
+        img : ""
+    },
+    {
+        name : "Idukki",
+        img : ""
+    },
+    {
+        name : "Ernakulam",
+        img : ""
+    },
+    {
+        name : "Thrissur",
+        img : ""
+    },
+    {
+        name : "Malappuram",
+        img : ""
+    },
+    {
+        name : "Kozhikode",
+        img : ""
+    },
+    {
+        name : "Wayanad",
+        img : ""
+    },
+    {
+        name : "Kannur",
+        img : ""
+    },
+    {
+        name : "Kasaragod",
+        img : ""
+    },
+
+    
+
+
+
+]
---
+++ src/assets/data.ts
@@ -0,0 +1,64 @@
+export const district = [
+    {
+        name : "Thiruvananthapuram",
+        img : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.tGBYvfeVzIT40QYycXXNsAHaEK%26pid%3DApi&f=1&ipt=cf0ebeb6c7775107afb8324f03500def5e6d7e953e1c7543e31bdd27da4d16d0&ipo=images"
+    },
+    {
+        name : "Kollam",
+        img : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.tripiwiki.com%2Fimages%2Fplaces%2Fuploads%2FKollam-Beach16136.jpg&f=1&nofb=1&ipt=b728cc16249630ef90ce86be9348bfdb55f5becffd8ea4693489b55506f9c94e&ipo=images"
+    },
+    {
+        name : "Pathanamthitta",
+        img : "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.justkerala.in%2Fwp-content%2Fuploads%2F2013%2F01%2Fchurch-in-Pathanamthitta.jpg&f=1&nofb=1&ipt=edf6dbbbd5b1357bc29ad96b8168d514c616ab00420c54676aa543c9b8dff8f3&ipo=images"
+    },
+    {
+        name : "Alappuzha",
+        img : "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Alappuzha_Boat_Beauty_W.jpg/1200px-Alappuzha_Boat_Beauty_W.jpg"
+    },
+    {
+        name : "Kottayam",
+        img : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.onmanorama.com%2Fcontent%2Fdam%2Fmm%2Fen%2Ftravel%2Freaders-discovery%2Fimages%2F2020%2F11%2F12%2Faruvikuzhy-waterfalls-2.jpg&f=1&nofb=1&ipt=90002e5594ef3c2fe64d7d065903007a1f5e0d418a7a8250f7cb7edb019e1087&ipo=images"
+    },
+    {
+        name : "Idukki",
+        img : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.zM14JKquXMwO1_P1duTuWgHaEK%26pid%3DApi&f=1&ipt=6b163f19758ffec0cae43477a4f52d7e9420387fe96872b666c2ad07b95411fe&ipo=images"
+    },
+    {
+        name : "Ernakulam",
+        img : "https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Kochi_Metro_train_at_Palarivattom%2C_Aug_2017.jpg/1200px-Kochi_Metro_train_at_Palarivattom%2C_Aug_2017.jpg"
+    },
+    {
+        name : "Thrissur",
+        img : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi1.wp.com%2Fwww.keralam.me%2Fwp-content%2Fuploads%2F2019%2F03%2Fthrissur_pooram.jpg%3Fzoom%3D3%26resize%3D800%252C445%26ssl%3D1&f=1&nofb=1&ipt=91d330ad5df35637a6b1929da88c90ac0d8c046076f6d11fb18f2e66f41199d0&ipo=images"
+    },
+    {
+        name : "Malappuram",
+        img : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.hgc93AvlYvSVWm2JnkjPWgHaE3%26pid%3DApi&f=1&ipt=fa895d2a806cdcea3cc6dc371847c8b89baa95820625891da910c4162ea53b9c&ipo=images"
+    },
+    {
+        name : "Palakkad",
+        img : "https://2.bp.blogspot.com/-q0l4H9YGgxY/VuaPsWHZ-5I/AAAAAAAAGTo/L346Uq14sM4y2qoUrWrenb_xWGUgHSs0QCPcB/s1600/IMG_4497.JPG"
+    },
+    {
+        name : "Kozhikode",
+        img : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fkerala2015.com%2Fsites%2Fdefault%2Ffiles%2Fstadiums%2FKozhikode-_Beach.jpg&f=1&nofb=1&ipt=cafcd77f4da0718937be27e86b19a8a16e9f3f5aed024d58b0be5aaef1f3abf8&ipo=images"
+    },
+    {
+        name : "Wayanad",
+        img : "https://www.ekeralatourism.net/wp-content/uploads/2018/12/things-wayanad2.jpg"
+    },
+    {
+        name : "Kannur",
+        img : "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Ficdn.images.touristlink.com%2Frepository%2FPA%2FRA%2FSS%2FIN%2FIE%2F7%2Fmuthappan-temple-parassini.jpg&f=1&nofb=1&ipt=a5199d25bf7a1705a9157b476a1b9260345863948997519187c8d2f22da36dd7&ipo=images"
+    },
+    {
+        name : "Kasaragod",
+        img : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fexperiencekerala.in%2Fimage-uploads%2F1468216282.Bakel_Fort_Kasaragod.jpg&f=1&nofb=1&ipt=827d6009395edfd5194f82d81959aa893bade52a3b684b32118a265efdcf0363&ipo=images"
+    },
+
+    
+
+
+
+]
+
